25506004094d864f88a079a043fc0d971eb5026e
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-11-07 20:48:07 +0530

    Event pages

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ let navUl = document.createElement('ul');
 navUl.innerHTML =  `<li class="nav-link event-links" id="event-links">
   <a href="#segfault">Events</a>
   <ul class="event-links-dropdown" id="event-links-dropdown">
-    <li><a href="#segfault">Segfault</a></li>
-    <li><a href="#rhythm-x">Rhythm-X</a></li>
-    <li><a href="#abstraction">Abstraction</a></li>
-    <li><a href="#maximus">Maximus</a></li>
-    <li><a href="#desolate-planet">Desolate Planet</a></li>
+    <li><a href="segfault.html">Segfault</a></li>
+    <li><a href="rhythm-x.html">Rhythm-X</a></li>
+    <li><a href="abstraction.html">Abstraction</a></li>
+    <li><a href="maximus.html">Maximus</a></li>
+    <li><a href="desolate-planet.html">Desolate Planet</a></li>
   </ul>
 </li>
-<li class="nav-link"><a href="#schedule">Schedule</a></li>
-<li class="nav-link"><a href="#registration">Registration</a></li>
-<li class="nav-link"><a href="#contact">Contact</a></li>`;
+<li class="nav-link"><a href="index.html#schedule">Schedule</a></li>
+<li class="nav-link"><a href="index.html#registration">Registration</a></li>
+<li class="nav-link"><a href="index.html#contact">Contact</a></li>`;
 navUl.className = 'flex';
 
 let events = navUl.querySelector('#event-links');
 let dropdown = navUl.querySelector('#event-links-dropdown');
 
 events.addEventListener('mouseenter', () => {
-  dropdown.style.height = '7rem';
+  dropdown.style.height = '11rem';
   dropdown.style.boxShadow = '0px 0px 20px 1px #0000001a';
   dropdown.style.opacity = 1;
 });
